98df62e222857a9ce0c2cf0f48545e1d1d545973
leofnch <leva.chizhow@gmail.com>
2018-03-11 12:50:00 +0300

    Browser

--- browser.py
+++ browser.py
@@ -1,6 +1,29 @@
-'''
- HodlML  Copyright (C) 2018 HODL - The Cryptocurrency
-This program comes with ABSOLUTELY NO WARRANTY;
-This is free software, and you are welcome to redistribute it
-under certain conditions;
-'''
+"""
+It is the browser for HODL markup.
+"""
+import sc_get
+import parser
+from objects import *
+from sc_net_api import *
+
+
+class Malware(Exception):
+    pass
+
+
+class Page:
+    def __init__(self, addr):
+        self.addr = addr
+        self.code = sc_get.get(addr.split('/')[0], '/'.join(addr.split('/')[1:]))
+        if not parser.secure(self.code):
+            raise Malware
+        self.root = HODLmlRootWidget()
+
+    def show(self):
+        self.elements = []
+        for line in self.code:
+            exec(line)
+        for e in self.elements:
+            self.root.add_widget(e)
+        return self.root
+
